8f616ca14a2eb21f84b576de57a1489c6e1e52ec
ohujj <114215140+ohujj@users.noreply.github.com>
2026-05-11 21:47:17 +0900

    init

---
+++ src/main/java/com/cmchackathon/CmchackathonApplication.java
@@ -0,0 +1,13 @@
+package com.cmchackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmchackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmchackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmchackathon/CmchackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmchackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmchackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
