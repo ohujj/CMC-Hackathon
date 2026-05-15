1f321a7e391f78d844576c6faf4a888b7d35e659
ohujj <114215140+ohujj@users.noreply.github.com>
2026-05-15 23:14:19 +0900

    TEST 배포 TEST

--- src/main/java/com/cmchackathon/testApi/TestEntity.java
+++ src/main/java/com/cmchackathon/testApi/TestEntity.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TestEntity extends BaseEntity {
-
+ 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
