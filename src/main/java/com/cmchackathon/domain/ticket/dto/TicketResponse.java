a7024342141d6d61de936edff929a10cff12079b
ohujj <114215140+ohujj@users.noreply.github.com>
2026-05-17 07:08:29 +0900

    닉네임 추가

--- src/main/java/com/cmchackathon/domain/ticket/dto/TicketResponse.java
+++ src/main/java/com/cmchackathon/domain/ticket/dto/TicketResponse.java
@@ -14,30 +14,31 @@ public class TicketResponse {
 
     private Long id;
     private Long movieSeq;
+    private String ownerNickname;
     private LocalDate watchedDate;
     private LocalTime watchedTime;
     private Integer rating;
     private String review;
     private boolean showYn;
     private boolean isLiked;
+    private long likeCount;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-    private long likeCount;
 
     public static TicketResponse from(Ticket ticket, boolean isLiked, long likeCount) {
         return new TicketResponse(
                 ticket.getId(),
                 ticket.getMovie().getSeq(),
+                ticket.getUser().getNickname(),
                 ticket.getWatchedDate(),
                 ticket.getWatchedTime(),
                 ticket.getRating(),
                 ticket.getReview(),
                 ticket.isShowYn(),
                 isLiked,
+                likeCount,
                 ticket.getCreatedAt(),
-                ticket.getUpdatedAt(),
-                likeCount
+                ticket.getUpdatedAt()
         );
     }
-
 }
