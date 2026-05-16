a1a762f643676aa3dbbb11e89aa4d76d60a43b8a
IISweetHeartII <sachi009955@gmail.com>
2026-05-16 22:17:02 +0900

    fix: update ticket domain imports after movie package move

--- src/main/java/com/cmchackathon/domain/ticket/entity/Ticket.java
+++ src/main/java/com/cmchackathon/domain/ticket/entity/Ticket.java
@@ -3,7 +3,7 @@ package com.cmchackathon.domain.ticket.entity;
 import com.cmchackathon.domain.ticket.dto.TicketUpdateRequest;
 import com.cmchackathon.domain.user.entity.User;
 import com.cmchackathon.global.entity.BaseEntity;
-import com.cmchackathon.movie.entity.Movie;
+import com.cmchackathon.domain.movie.entity.Movie;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
